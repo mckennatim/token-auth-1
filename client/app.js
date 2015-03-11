78542d33c4d9f6c85bb3bd0f1f366537ed6aaf8f
mckennatim <mckenna.tim@gmail.com>
2015-03-11 12:09:17 -0400

    maac edits

--- client/app.js
+++ client/app.js
@@ -7,13 +7,34 @@ var app = angular.module("App", [
     ]);
 
 app.constant('cfg', {
-    serverUrl: 'http://localhost:3004/api/',
-    // 'http://parleyvale.com:3000/api/'  'http://sitebuilt.net:3000/api/'
-    LSpre: 'reg_',
+    setup: function(){
+        var port = 3002;
+        var url = 'http://localhost:'+port+'/api/';
+        var prefix = 'reg_';
+        var toState = 'lists'
+        return {
+            port: port,
+            url: url,
+            prefix: prefix,
+            toState: toState
+        }
+    },
     afterReg: function(user){
         console.log(user + ' is registered w/token')
     }
 })
+
+app.config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider){
+    $stateProvider.state('splash',{
+        url:'/',
+        template: '<h4>Splash</h4>'
+    });
+    $stateProvider.state('lists',{
+        url:'/lists',
+        template: '<h4>Lists</h4>'
+    });
+    $urlRouterProvider.otherwise('/');    
+}])
 
 app.config(function ($httpProvider) {
     $httpProvider.interceptors.push('OnlineInterceptor');
